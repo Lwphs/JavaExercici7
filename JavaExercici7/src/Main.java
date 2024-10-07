5053b221590281a617b6eae915c4767d10cc28b0
Lwphs <arnau.garcia@gracia.lasalle.cat>
2024-10-07 17:18:47 +0200

    Update Main.java

--- JavaExercici7/src/Main.java
+++ JavaExercici7/src/Main.java
@@ -8,36 +8,47 @@ public class Main {
 
         String extra_hours = "";
         double nHours = 0;
-        double extra_pay = 0:
+        double extra_pay = 0;
         double normal_pay = 1250;
 
         System.out.println("Hello, your base salary is 1250€.");
-        System.out.print("Have you done any extra hours? Yes / No");
-        extra_hours = input.next();
-        input.nextLine();
+        System.out.println("Have you done any extra hours? Yes / No");
+        extra_hours = input.nextLine();
+
 
         while (!extra_hours.equalsIgnoreCase("Yes") && !extra_hours.equalsIgnoreCase("No")) {
             System.out.println("Please, input a valid answer (Yes/No)");
-            extra_hours = input.next();
+            extra_hours = input.nextLine();
         }
 
         switch (extra_hours){
             case "Yes":
             case "yes":
                 System.out.println("Please, enter the number of extra hours done");
+
+                while (!input.hasNextDouble()) {
+                    System.out.println("Please, input a valid answer");
+                    input.next();
+                }
+
                 nHours = input.nextDouble();
-                input.nextLine();
 
-                while (!extra_hours.hasNextDouble) {
-                    System.out.println("Please, input a valid answer);
-                    nHours = input.next();
-                }
 
-                if (nHours < 5 && nHours > 0){
-                    
-                }
+                if (nHours <= 5 && nHours >= 0){
+                    extra_pay = nHours * 15;
+                } else if (nHours > 5) {
+                    extra_pay = 5 * 15 + (nHours - 5) * 12;
+                } else
+                    System.out.println("Invalid value.");
+                break;
+
+            case "No":
+            case "no":
+                extra_pay = 0;
                 break;
         }
+
+        System.out.println("Your salary is "+ (extra_pay + normal_pay) + " euros.");
 
 
     }
